c0e921ccd53cf2e6268c6732db01dd973c65e83b
Pedro <pedrohcm1993@hotmail.com>
2021-10-15 09:03:34 -0300

    Salvandoo projeto web-npm-git-test rep local

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+var activenav = document.getElementById("navmenu")
+// Criou uma variavel que já pega o id
+
+function openMenu() {
+    activenav.style.width = "200px";
+}
+
+function closeMenu() {
+    activenav.style.width = "0px";
+}
